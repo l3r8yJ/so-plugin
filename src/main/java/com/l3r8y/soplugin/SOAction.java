5f625180ded016a71dc1d39c4e7a58534e36b3a4
Ivan <clicker.heroes.acg@gmail.com>
2022-11-14 22:37:17 +0300

    refactor

--- src/main/java/com/l3r8y/soplugin/SOAction.java
+++ src/main/java/com/l3r8y/soplugin/SOAction.java
@@ -33,23 +33,27 @@ import java.util.Locale;
 import java.util.Optional;
 import org.jetbrains.annotations.NotNull;
 
-// https://stackoverflow.com/questions/ask
+
 public class SOAction extends AnAction {
 
   @Override
   public final void actionPerformed(@NotNull final AnActionEvent evt) {
     final Optional<Editor> editor = Optional.ofNullable(evt.getData(CommonDataKeys.EDITOR));
     final Optional<Language> lang = Optional.ofNullable(evt.getData(CommonDataKeys.LANGUAGE));
-    editor.ifPresent(
-      edtr -> {
-        final String code = edtr.getSelectionModel().getSelectedText();
-        final Optional<String> ext = lang.map(lng -> lng.getDisplayName().toLowerCase(Locale.ROOT));
-        if (ext.isPresent() && null != code) {
-          Messages.showMessageDialog("Done!", "SO-Plugin", Messages.getInformationIcon());
-          BrowserUtil.browse(SOAction.buildQuery(code, ext.get()));
-        }
-      }
-    );
+    editor.ifPresent(edtr-> SOAction.openBrowserIfSelectedCode(lang, edtr));
+  }
+
+  private static void openBrowserIfSelectedCode(final Optional<Language> lang, final Editor edtr) {
+    final String code = edtr.getSelectionModel().getSelectedText();
+    final Optional<String> ext = lang.map(lng -> lng.getDisplayName().toLowerCase(Locale.ROOT));
+    if (ext.isPresent() && null != code) {
+      Messages.showMessageDialog(
+        "Done!",
+        "SO-Plugin",
+        Messages.getInformationIcon()
+      );
+      BrowserUtil.browse(SOAction.buildQuery(code, ext.get()));
+    }
   }
 
   private static String buildQuery(final String code, final String ext) {
